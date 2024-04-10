4c3cf5052dc57c0da547efaae53dbbe005256054
HisaichiKouki <k023g0068@g.neec.ac.jp>
2024-04-11 00:10:01 +0900

    AL3_01_01

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	win = WinApp::GetInstance();
 	win->CreateGameWindow(L"LD2A_01_ヒサイチ_コウキ_AL3");
 
+
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
